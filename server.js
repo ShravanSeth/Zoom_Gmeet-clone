83bc43b6e0adf63acffe7355a5516756f7754919
Shravan Seth <64133116+ShravanSeth@users.noreply.github.com>
2021-06-16 16:31:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,30 +2,38 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+	debug: true,
+})
+const { v4: uuidv4 } = require('uuid')
 
-app.set('view engine', 'ejs')
+app.use('/peerjs', peerServer)
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+	res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+	res.render('room', { roomId: req.params.room })
 })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+io.on('connection', (socket) => {
+	socket.on('join-room', (roomId, userId) => {
+		socket.join(roomId)
+		socket.to(roomId).broadcast.emit('user-connected', userId)
+
     socket.join(roomId)
     socket.broadcast.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
       socket.broadcast.to(roomId).emit('user-disconnected', userId)
     })
-  })
+	})
 })
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
